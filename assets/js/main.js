525cdffda47cfc58e498f38f3b7facd41fd25095
jjub0217 <noonssub1004@gmail.com>
2023-11-23 00:09:52 +0900

    메뉴리스트 호버시 서브리스트 나타나는 것과 푸터 연관사이트 나타내기 기능 수정중

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,11 @@
 // 브라우저가 로드 되면 바로 시작해줘러.
 $(function () {
 
-    new Swiper('.main-visual .swiper',{
+   const mainVisual = new Swiper('.section-visual .swiper',{
+    autoplay:{
+      delay: 5000,
+    },
+    loop: true,
     navigation: {
       nextEl: ".btn-next",
       prevEl: ".btn-prev"
@@ -10,6 +14,37 @@ $(function () {
       el: ".pagination",
       clickable: true
     },
+  })
 
+  $(".btn-autoplay").click(function(){
+    if($(this).hasClass("on")){
+      $(this).removeClass("on")
+      mainVisual.autoplay.start();
+    }else{
+      $(this).addClass("on")
+      mainVisual.autoplay.stop();
+    }
+  })
+
+  $(".auth-area > li").hover(function(){
+    $(this).find(".sub-list").addClass("on")
+  },function(){
+    $(this).find('.sub-list').removeClass('on');
+  })
+
+  $(".gnb .nav-item").hover(function(){
+  $(this).find(".sub-list").addClass("on")
+  $(this).find(".triangle").addClass("on")
+  }, function () {
+  $(this).find('.sub-list').removeClass('on');
+  $(this).find(".triangle").removeClass("on")
+  })
+
+  $(".util-list .util-item").hover(function(){
+  $(this).find(".sub-list").addClass("on")
+  $(this).find(".triangle").addClass("on")
+  }, function () {
+  $(this).find('.sub-list').removeClass('on');
+  $(this).find(".triangle").removeClass("on")
   })
 })
